87f3d495523efefb3bc345cce941208a589e4028
Thad House <thadhouse1@gmail.com>
2022-03-06 13:35:39 -0800

    Formatting

--- common/QuicConnection.cpp
+++ common/QuicConnection.cpp
@@ -24,7 +24,7 @@ struct QuicConnection::Impl
 
     QUIC_STATUS ConnCallback(QUIC_CONNECTION_EVENT *Event);
     QUIC_STATUS StreamCallback(QUIC_STREAM_EVENT *Event);
-    QUIC_STATUS ListenerCallback(QUIC_LISTENER_EVENT* Event);
+    QUIC_STATUS ListenerCallback(QUIC_LISTENER_EVENT *Event);
 
     ~Impl() noexcept
     {
@@ -38,10 +38,12 @@ struct QuicConnection::Impl
             MsQuic->ConnectionShutdown(Connection, QUIC_CONNECTION_SHUTDOWN_FLAG_NONE, 0);
             MsQuic->ConnectionClose(Connection);
         }
-        if (Configuration) {
+        if (Configuration)
+        {
             MsQuic->ConfigurationClose(Configuration);
         }
-        if (Listener) {
+        if (Listener)
+        {
             MsQuic->ListenerStop(Listener);
             MsQuic->ListenerClose(Listener);
         }
@@ -64,12 +66,13 @@ void *QuicConnection::GetConnectionHandle() noexcept
 
 void QuicConnection::WriteDatagram(wpi::span<uint8_t> datagram)
 {
-    QUIC_BUFFER* Buffer = (QUIC_BUFFER*)malloc(sizeof(QUIC_BUFFER) + datagram.size());
-    Buffer->Buffer = (uint8_t*)(Buffer + 1);
+    QUIC_BUFFER *Buffer = (QUIC_BUFFER *)malloc(sizeof(QUIC_BUFFER) + datagram.size());
+    Buffer->Buffer = (uint8_t *)(Buffer + 1);
     Buffer->Length = datagram.size();
     std::memcpy(Buffer->Buffer, datagram.data(), datagram.size());
     QUIC_STATUS Status = MsQuic->DatagramSend(pImpl->Connection, Buffer, 1, QUIC_SEND_FLAG_DGRAM_PRIORITY, Buffer);
-    if (QUIC_FAILED(Status)) {
+    if (QUIC_FAILED(Status))
+    {
         printf("Datagram failed to send\n");
         free(Buffer);
     }
@@ -77,12 +80,13 @@ void QuicConnection::WriteDatagram(wpi::span<uint8_t> datagram)
 
 void QuicConnection::WriteStream(wpi::span<uint8_t> data)
 {
-    QUIC_BUFFER* Buffer = (QUIC_BUFFER*)malloc(sizeof(QUIC_BUFFER) + data.size());
-    Buffer->Buffer = (uint8_t*)(Buffer + 1);
+    QUIC_BUFFER *Buffer = (QUIC_BUFFER *)malloc(sizeof(QUIC_BUFFER) + data.size());
+    Buffer->Buffer = (uint8_t *)(Buffer + 1);
     Buffer->Length = data.size();
     std::memcpy(Buffer->Buffer, data.data(), data.size());
     QUIC_STATUS Status = MsQuic->StreamSend(pImpl->Stream, Buffer, 1, QUIC_SEND_FLAG_NONE, Buffer);
-    if (QUIC_FAILED(Status)) {
+    if (QUIC_FAILED(Status))
+    {
         printf("Stream failed to send\n");
         free(Buffer);
     }
@@ -171,18 +175,17 @@ QuicConnection::QuicConnection(std::string Host, uint16_t Port)
     }
 
     BOOLEAN value = TRUE;
-        Status =
-            MsQuic->SetParam(
-                pImpl->Connection,
-                QUIC_PARAM_CONN_DISABLE_1RTT_ENCRYPTION,
-                sizeof(value),
-                &value);
+    Status =
+        MsQuic->SetParam(
+            pImpl->Connection,
+            QUIC_PARAM_CONN_DISABLE_1RTT_ENCRYPTION,
+            sizeof(value),
+            &value);
 
-                if (QUIC_FAILED(Status))
+    if (QUIC_FAILED(Status))
     {
         throw std::runtime_error("Failed to disable encryption");
     }
-
 
     Status = MsQuic->StreamOpen(pImpl->Connection, QUIC_STREAM_OPEN_FLAG_NONE, StreamCallback, pImpl.get(), &pImpl->Stream);
     if (QUIC_FAILED(Status))
@@ -206,7 +209,8 @@ QuicConnection::QuicConnection(std::string Host, uint16_t Port)
     }
 }
 
-QuicConnection::QuicConnection(uint16_t Port) {
+QuicConnection::QuicConnection(uint16_t Port)
+{
     pImpl = std::make_unique<QuicConnection::Impl>(this);
 
     QUIC_SETTINGS Settings;
@@ -268,24 +272,35 @@ QuicConnection::QuicConnection(uint16_t Port) {
 
 QuicConnection::QuicConnection() noexcept = default;
 
-void QuicConnection::Disconnect() {
-    if (pImpl->Stream) {
+void QuicConnection::Disconnect()
+{
+    if (pImpl->Stream)
+    {
         MsQuic->StreamShutdown(pImpl->Stream, QUIC_STREAM_SHUTDOWN_FLAG_NONE, 0);
     }
 }
 
-QUIC_STATUS QuicConnection::Impl::ConnCallback(QUIC_CONNECTION_EVENT *Event) {
-    if (Event->Type == QUIC_CONNECTION_EVENT_CONNECTED) {
-        if (Stream != nullptr) {
+QUIC_STATUS QuicConnection::Impl::ConnCallback(QUIC_CONNECTION_EVENT *Event)
+{
+    if (Event->Type == QUIC_CONNECTION_EVENT_CONNECTED)
+    {
+        if (Stream != nullptr)
+        {
             Owner->ReadyEvent.Set();
         }
-    } else if (Event->Type == QUIC_CONNECTION_EVENT_PEER_STREAM_STARTED) {
-        MsQuic->SetCallbackHandler(Event->PEER_STREAM_STARTED.Stream, (void*)::StreamCallback, this);
+    }
+    else if (Event->Type == QUIC_CONNECTION_EVENT_PEER_STREAM_STARTED)
+    {
+        MsQuic->SetCallbackHandler(Event->PEER_STREAM_STARTED.Stream, (void *)::StreamCallback, this);
         Stream = Event->PEER_STREAM_STARTED.Stream;
         Owner->ReadyEvent.Set();
-    } else if (Event->Type == QUIC_CONNECTION_EVENT_SHUTDOWN_COMPLETE) {
+    }
+    else if (Event->Type == QUIC_CONNECTION_EVENT_SHUTDOWN_COMPLETE)
+    {
         Owner->DisconnectedEvent.Set();
-    } else if (Event->Type == QUIC_CONNECTION_EVENT_DATAGRAM_RECEIVED) {
+    }
+    else if (Event->Type == QUIC_CONNECTION_EVENT_DATAGRAM_RECEIVED)
+    {
         DatagramBuffer datagram;
         datagram.Length = Event->DATAGRAM_RECEIVED.Buffer->Length;
         datagram.Buffer = std::make_unique<uint8_t[]>(datagram.Length);
@@ -294,31 +309,43 @@ QUIC_STATUS QuicConnection::Impl::ConnCallback(QUIC_CONNECTION_EVENT *Event) {
         std::scoped_lock Lock{Owner->DatagramMutex};
         Owner->DatagramData.emplace_back(std::move(datagram));
         Owner->DatagramEvent.Set();
-    } else if (Event->Type == QUIC_CONNECTION_EVENT_DATAGRAM_SEND_STATE_CHANGED) {
-        if (QUIC_DATAGRAM_SEND_STATE_IS_FINAL(Event->DATAGRAM_SEND_STATE_CHANGED.State)) {
+    }
+    else if (Event->Type == QUIC_CONNECTION_EVENT_DATAGRAM_SEND_STATE_CHANGED)
+    {
+        if (QUIC_DATAGRAM_SEND_STATE_IS_FINAL(Event->DATAGRAM_SEND_STATE_CHANGED.State))
+        {
             free(Event->DATAGRAM_SEND_STATE_CHANGED.ClientContext);
         }
     }
     return QUIC_STATUS_SUCCESS;
 }
 
-QUIC_STATUS QuicConnection::Impl::StreamCallback(QUIC_STREAM_EVENT *Event) {
-    if (Event->Type == QUIC_STREAM_EVENT_SHUTDOWN_COMPLETE) {
+QUIC_STATUS QuicConnection::Impl::StreamCallback(QUIC_STREAM_EVENT *Event)
+{
+    if (Event->Type == QUIC_STREAM_EVENT_SHUTDOWN_COMPLETE)
+    {
         MsQuic->ConnectionShutdown(Connection, QUIC_CONNECTION_SHUTDOWN_FLAG_NONE, 0);
-    } else if (Event->Type == QUIC_STREAM_EVENT_RECEIVE) {
+    }
+    else if (Event->Type == QUIC_STREAM_EVENT_RECEIVE)
+    {
         std::scoped_lock Lock{Owner->StreamMutex};
-        for (uint32_t i = 0; i < Event->RECEIVE.BufferCount; i++) {
+        for (uint32_t i = 0; i < Event->RECEIVE.BufferCount; i++)
+        {
             Owner->StreamData.insert(Owner->StreamData.end(), Event->RECEIVE.Buffers[i].Buffer, Event->RECEIVE.Buffers[i].Buffer + Event->RECEIVE.Buffers[i].Length);
         }
         Owner->StreamEvent.Set();
-    } else if (Event->Type == QUIC_STREAM_EVENT_SEND_COMPLETE) {
+    }
+    else if (Event->Type == QUIC_STREAM_EVENT_SEND_COMPLETE)
+    {
         free(Event->SEND_COMPLETE.ClientContext);
     }
     return QUIC_STATUS_SUCCESS;
 }
 
-QUIC_STATUS QuicConnection::Impl::ListenerCallback(QUIC_LISTENER_EVENT *Event) {
-    if (Event->Type == QUIC_LISTENER_EVENT_NEW_CONNECTION) {
+QUIC_STATUS QuicConnection::Impl::ListenerCallback(QUIC_LISTENER_EVENT *Event)
+{
+    if (Event->Type == QUIC_LISTENER_EVENT_NEW_CONNECTION)
+    {
         MsQuic->ListenerStop(Listener);
         BOOLEAN value = TRUE;
         MsQuic->SetParam(
@@ -326,7 +353,7 @@ QUIC_STATUS QuicConnection::Impl::ListenerCallback(QUIC_LISTENER_EVENT *Event) {
             QUIC_PARAM_CONN_DISABLE_1RTT_ENCRYPTION,
             sizeof(value),
             &value);
-        MsQuic->SetCallbackHandler(Event->NEW_CONNECTION.Connection, (void*)::ConnCallback, this);
+        MsQuic->SetCallbackHandler(Event->NEW_CONNECTION.Connection, (void *)::ConnCallback, this);
         Connection = Event->NEW_CONNECTION.Connection;
         QUIC_STATUS Status = MsQuic->ConnectionSetConfiguration(Event->NEW_CONNECTION.Connection, Configuration);
         return Status;
