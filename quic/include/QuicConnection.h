62aa21ccd72ad27ca222ebf80a7779ccad44ffbe
Thad House <thadhouse1@gmail.com>
2022-03-15 22:31:02 -0700

    Most of listener side done

--- quic/include/QuicConnection.h
+++ quic/include/QuicConnection.h
@@ -21,10 +21,26 @@ typedef int32_t QuicConnStatus;
 typedef uint32_t QuicConnBoolean;
 
 typedef QuicConnBoolean(QUIC_CONN_API * QuicListenerNewConnectionCallback)(QuicConnection* Connection, void* Context);
+typedef void(QUIC_CONN_API * QuicListenerStoppedCallback)(void* Context);
 typedef void(QUIC_CONN_API * QuicConnectionReadyCallback)(void* Context);
 typedef void(QUIC_CONN_API * QuicConnectionDisconnectedCallback)(void* Context);
 typedef void(QUIC_CONN_API * QuicConnectionDatagramDataCallback)(void* Context, const QuicDataBuffer* DataBuffer);
 typedef void(QUIC_CONN_API * QuicConnectionStreamDataCallback)(uint32_t StreamId, void* Context, const QuicDataBuffer* DataBuffers, uint32_t DataBuffersSize);
+
+typedef struct QuicListenerCallbacks {
+    QuicListenerNewConnectionCallback NewConnectionCallback;
+    QuicListenerStoppedCallback StoppedCallback;
+    void* Context;
+} QuicListenerCallbacks;
+
+typedef struct QuicConnectionCallbacks {
+    QuicConnectionReadyCallback NewConnectionCallback;
+    QuicConnectionDisconnectedCallback StoppedCallback;
+    QuicConnectionDatagramDataCallback DatagramDataCallback;
+    QuicConnectionStreamDataCallback StreamDataCallback;
+
+    void* Context;
+} QuicConnectionCallbacks;
 
 #ifdef __cplusplus
 extern "C" {
@@ -33,11 +49,15 @@ extern "C" {
 QuicConnStatus QC_GetRegistration(const char* Name, QuicConnBoolean UseSingleThread, QuicRegistration** Registration);
 void QC_FreeRegistration(QuicRegistration* Registration);
 
-QuicConnStatus QC_CreateListener(const QuicRegistration* Registration, uint16_t Port, uint8_t* PfxBuffer, uint32_t PfxSize, const char* PfxPassword, uint32_t NumStreams, QuicListenerNewConnectionCallback Callback, void* Context, QuicListener** Listener);
-void QC_StartListener(QuicListener* Listener);
+QuicConnStatus QC_CreateListener(const QuicRegistration* Registration, uint16_t Port, uint8_t* Alpn, uint16_t AlpnLength, uint8_t* PfxBuffer, uint32_t PfxSize, const char* PfxPassword, uint32_t NumStreams, QuicListenerCallbacks* Callbacks, QuicListener** Listener);
+QuicConnStatus QC_StartListener(QuicListener* Listener);
 void QC_StopListener(QuicListener* Listener);
+
+// Listener must be stopped, and QuicListenerStopped event must have been received in order to
+// call this API. Additionally, all connections received from the listener must have been freed.
 void QC_FreeListener(QuicListener* Listener);
 
+void QC_SetConnectionContext(QuicConnection* Connection, QuicConnectionCallbacks* Callbacks);
 void QC_ShutdownConnection(QuicConnection* Connection);
 void QC_FreeConnection(QuicConnection* Connection);
 
--- quic/src/QuicConnection.c
+++ quic/src/QuicConnection.c
@@ -6,9 +6,12 @@
 // warning here. This is not an MsQuic bug but a Windows SDK bug.
 //
 #pragma warning(disable:5105)
+#pragma warning(disable:4200)  // nonstandard extension used: zero-sized array in struct/union
 #endif
 #include "msquic.h"
 #include <stdlib.h>
+
+#define BYTESWAPSHORT(x) ((uint16_t)((((x) & 0x00ff) << 8) | (((x) & 0xff00) >> 8)))
 
 struct QuicRegistration {
     const QUIC_API_TABLE* QuicApi;
@@ -18,6 +21,20 @@ struct QuicRegistration {
 struct QuicListener {
     const QuicRegistration* Registration;
     HQUIC Listener;
+    HQUIC Configuration;
+    QuicListenerCallbacks Callbacks;
+    uint32_t NumStreams;
+    uint16_t Port;
+    uint16_t AlpnLength;
+    uint8_t Alpn[0];
+};
+
+struct QuicConnection {
+    const QuicRegistration* Registration;
+    HQUIC Connection;
+    QuicConnectionCallbacks Callbacks;
+    uint32_t NumStreams;
+    HQUIC Streams[0];
 };
 
 QuicConnStatus QC_GetRegistration(const char* Name, QuicConnBoolean UseSingleThread, QuicRegistration** Registration) {
@@ -71,14 +88,50 @@ void QC_FreeRegistration(QuicRegistration* Registration) {
     }
 }
 
-QuicConnStatus QC_CreateListener(const QuicRegistration* Registration, uint16_t Port, uint8_t* PfxBuffer, uint32_t PfxSize, const char* PfxPassword, uint32_t NumStreams, QuicListenerNewConnectionCallback Callback, void* Context, QuicListener** Listener) {
+static QUIC_STATUS QUIC_API ListenerCallback(HQUIC Listener, void* Context, QUIC_LISTENER_EVENT* Event) {
+    (void)Listener;
+    QuicListener* QListener = (QuicListener*)Context;
+
+    if (Event->Type == QUIC_LISTENER_EVENT_NEW_CONNECTION) {
+        QuicConnection* NewConnection = malloc(sizeof(QuicConnection) + QListener->NumStreams);
+        if (!NewConnection) {
+            return QUIC_STATUS_OUT_OF_MEMORY;
+        }
+        memset(NewConnection, 0, sizeof(*NewConnection) + QListener->NumStreams);
+        NewConnection->NumStreams = QListener->NumStreams;
+        NewConnection->Registration = QListener->Registration;
+        NewConnection->Connection = Event->NEW_CONNECTION.Connection;
+
+        QUIC_STATUS Status = QListener->Registration->QuicApi->ConnectionSetConfiguration(NewConnection->Connection, QListener->Configuration);
+        if(QUIC_FAILED(Status)) {
+            free(NewConnection);
+            return Status;
+        }
+
+        QuicConnBoolean KeepConnection = QListener->Callbacks.NewConnectionCallback(NewConnection, QListener->Callbacks.Context);
+        if (!KeepConnection) {
+            free(NewConnection);
+            return QUIC_STATUS_INVALID_STATE;
+        }
+    } else if (Event->Type == QUIC_LISTENER_EVENT_STOP_COMPLETE) {
+        QListener->Callbacks.StoppedCallback(QListener->Callbacks.Context);
+    }
+
+    return QUIC_STATUS_SUCCESS;
+}
+
+QuicConnStatus QC_CreateListener(const QuicRegistration* Registration, uint16_t Port, uint8_t* Alpn, uint16_t AlpnLength, uint8_t* PfxBuffer, uint32_t PfxSize, const char* PfxPassword, uint32_t NumStreams, QuicListenerCallbacks* Callbacks, QuicListener** Listener) {
     QUIC_STATUS Status;
-    QuicListener* NewListener = malloc(sizeof(QuicListener));
+    QuicListener* NewListener = malloc(sizeof(QuicListener) + AlpnLength);
     if (!NewListener) {
         return QUIC_STATUS_OUT_OF_MEMORY;
     }
     memset(NewListener, 0, sizeof(*NewListener));
     NewListener->Registration = Registration;
+    NewListener->NumStreams = NumStreams;
+    NewListener->Port = Port;
+    NewListener->AlpnLength = AlpnLength;
+    memcpy(NewListener->Alpn, Alpn, AlpnLength);
 
     QUIC_CREDENTIAL_CONFIG CredConfig;
     memset(&CredConfig, 0, sizeof(CredConfig));
@@ -90,8 +143,37 @@ QuicConnStatus QC_CreateListener(const QuicRegistration* Registration, uint16_t 
 
     CredConfig.CertificatePkcs12 = &Pkcs12;
 
-    Status = Registration->QuicApi->ConfigurationOpen(Registration->Registration, )
+    QUIC_BUFFER AlpnBuffer;
+    AlpnBuffer.Buffer = NewListener->Alpn;
+    AlpnBuffer.Length = NewListener->AlpnLength;
 
+    QUIC_SETTINGS Settings;
+    Settings.IsSetFlags = 0;
+    Settings.IsSet.PeerBidiStreamCount = 1;
+    Settings.PeerBidiStreamCount = (uint16_t)NumStreams;
+    Settings.IsSet.DatagramReceiveEnabled = 1;
+    Settings.DatagramReceiveEnabled = 1;
+    Settings.IsSet.IdleTimeoutMs = 1;
+    Settings.IdleTimeoutMs = 2000; // TODO make this configurable
+
+    Status = Registration->QuicApi->ConfigurationOpen(Registration->Registration, &AlpnBuffer, 1, &Settings, sizeof(Settings), NewListener, &NewListener->Configuration);
+    if (QUIC_FAILED(Status)) {
+        goto Exit;
+    }
+
+    Status = Registration->QuicApi->ConfigurationLoadCredential(NewListener->Configuration, &CredConfig);
+    if (QUIC_FAILED(Status)) {
+        goto Exit;
+    }
+
+    Status = Registration->QuicApi->ListenerOpen(Registration->Registration, ListenerCallback, NewListener, &NewListener->Listener);
+    if (QUIC_FAILED(Status)) {
+        goto Exit;
+    }
+
+    NewListener->Callbacks = *Callbacks;
+
+    Status = QUIC_STATUS_SUCCESS;
     *Listener = NewListener;
     NewListener = NULL;
 
@@ -100,16 +182,59 @@ Exit:
         if (NewListener->Listener) {
             NewListener->Registration->QuicApi->ListenerClose(NewListener->Listener);
         }
+        if (NewListener->Configuration) {
+            NewListener->Registration->QuicApi->ConfigurationClose(NewListener->Configuration);
+        }
     }
 
     return Status;
 }
 
-void QC_StartListener(QuicListener* Listener);
-void QC_StopListener(QuicListener* Listener);
+QuicConnStatus QC_StartListener(QuicListener* Listener) {
+    QUIC_BUFFER AlpnBuffer;
+    AlpnBuffer.Buffer = Listener->Alpn;
+    AlpnBuffer.Length = Listener->AlpnLength;
+    QUIC_ADDR QuicAddr;
+    memset(&QuicAddr, 0, sizeof(QuicAddr));
+    QuicAddr.Ipv4.sin_port = BYTESWAPSHORT(Listener->Port);
+    return Listener->Registration->QuicApi->ListenerStart(Listener->Listener, &AlpnBuffer, 1, &QuicAddr);
+}
+
+void QC_StopListener(QuicListener* Listener) {
+    Listener->Registration->QuicApi->ListenerStop(Listener->Listener);
+}
 
 void QC_FreeListener(QuicListener* Listener) {
     if (Listener) {
         Listener->Registration->QuicApi->ListenerClose(Listener->Listener);
+        Listener->Registration->QuicApi->ConfigurationClose(Listener->Configuration);
+        free(Listener);
+    }
+}
+
+static QUIC_STATUS QUIC_API ConnectionCallback(HQUIC Connection, void* Context, QUIC_CONNECTION_EVENT* Event) {
+    (void)Connection;
+    (void)Context;
+    (void)Event;
+
+    return QUIC_STATUS_SUCCESS;
+}
+
+void QC_SetConnectionContext(QuicConnection* Connection, QuicConnectionCallbacks* Callbacks) {
+    Connection->Callbacks = *Callbacks;
+    Connection->Registration->QuicApi->SetCallbackHandler(Connection->Connection, (void*)ConnectionCallback, Connection);
+}
+
+void QC_ShutdownConnection(QuicConnection* Connection) {
+    Connection->Registration->QuicApi->ConnectionShutdown(Connection->Connection, QUIC_CONNECTION_SHUTDOWN_FLAG_NONE, 0);
+}
+
+void QC_FreeConnection(QuicConnection* Connection) {
+    if (Connection) {
+        for (uint32_t i = 0; i < Connection->NumStreams; i++) {
+            Connection->Registration->QuicApi->StreamClose(Connection->Streams[i]);
+        }
+        Connection->Registration->QuicApi->ConnectionClose(Connection->Connection);
+        free(Connection);
     }
 }
